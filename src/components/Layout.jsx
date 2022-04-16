db5af75afb9c21c312d0f4b2d98e6b16897aa16c
agilmanf <agilmanf@gmail.com>
2022-04-16 17:53:12 +0700

    add sidebar & song player

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,64 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import React, { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getToken } from "../redux/actions/spotifyAccess.action";
+
+// Components ///
 import Navbar from "./Navbar";
+import Player from "./Player";
 import Sidebar from "./Sidebar";
 
+// Style //
+import "./layout.css";
+
+// Get Spotify Code //
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=ba70ac6a4a564aa980ecfbbe38450255&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+const code = new URLSearchParams(window.location.search).get("code");
+
 function Layout() {
+  const [userLogin, setUserLogin] = useState({});
+  const navigate = useNavigate();
+
+  // Get Spotify Access Token //
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.spotifyAccessReducer);
+
+  useEffect(() => {
+    const regex = /(?<=token=).*$/g;
+    const loginToken = regex.exec(document.cookie);
+
+    // check if user is login
+    if (loginToken) {
+      setUserLogin(jwtDecode(loginToken[0]));
+    } else {
+      navigate("/login");
+    }
+  }, []);
+
+  // check if have access to spotify api
+  useEffect(() => {
+    if (token.accessToken === "" && !code)
+      return (window.location.href = AUTH_URL);
+    else if (token.accessToken == "") dispatch(getToken(code));
+  }, [code]);
+
+  //////////////////////////////////////
   return (
     <>
-      <div className="d-flex">
+      <div className="d-flex" style={{ position: "relative" }}>
         <Sidebar />
-        <div className="d-flex flex-column w-100">
+        <div
+          className="d-flex flex-column w-100"
+          style={{ position: "absolute", zIndex: 10 }}
+        >
           <Navbar />
-          <Outlet />
+          <div className="px-5 py-4" style={{ margin: "50px 0 50px 200px" }}>
+            <Outlet />
+          </div>
         </div>
+        <Player />
       </div>
     </>
   );
